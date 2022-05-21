af3b14ececbc7dac185af91f80c92fcead70a069
hector vargas <hlvm03@gmail.com>
2022-05-20 21:08:08 -0500

    adding secret

--- services/getIpClient.js
+++ services/getIpClient.js
@@ -1,5 +1,6 @@
 export default function getIpClient(req) {
-   if (process.env.NODE_ENV !== 'production') return '181.66.142.244'
+   const secret = process.env.TRIPLE_K
+   if (process.env.NODE_ENV !== 'production') return secret
    const ip = req?.headers['x-forwarded-for'] || req?.headers['x-real-ip']
    return ip?.includes('::ffff:') ? ip.slice(7) : ip
 }
