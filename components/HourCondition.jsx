a911f5107554409ed6369103a1e104519986091c
hector vargas <hlvm03@gmail.com>
2022-05-22 15:21:38 -0500

    adding hour component

---
+++ components/HourCondition.jsx
@@ -0,0 +1,16 @@
+import { Box } from '@mantine/core'
+
+export default function HourCondition() {
+   return (
+      <Box
+         sx={{
+            borderRadius: 999999,
+            width: 60,
+            height: 146,
+            background: '#48319D',
+            border: '1px solid #ffffff4f',
+            boxShadow: '5px 4px 10px rgb(0 0 0 / 25%)'
+         }}
+      ></Box>
+   )
+}
