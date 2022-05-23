c722f15b91db7d0dab99e23e0bb669f9761ff02d
hector vargas <hlvm03@gmail.com>
2022-05-23 14:26:01 -0500

    phill hour condition component

--- components/HourCondition.jsx
+++ components/HourCondition.jsx
@@ -9,8 +9,29 @@ export default function HourCondition() {
             height: 146,
             background: '#48319D',
             border: '1px solid #ffffff4f',
-            boxShadow: '5px 4px 10px rgb(0 0 0 / 25%)'
+            boxShadow: '5px 4px 10px rgb(0 0 0 / 25%)',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            color: 'white',
+            userSelect: 'none'
          }}
-      ></Box>
+      >
+         <Box component="time" children="12 AM" />
+         <Box
+            sx={{
+               background: 'url(/wi/179.svg)',
+               width: '100%',
+               height: 64,
+               backgroundRepeat: 'no-repeat'
+            }}
+         />
+         <Box
+            sx={{ lineHeight: 1, fontSize: '1.2rem' }}
+            component="time"
+            children="19°"
+         />
+      </Box>
    )
 }
