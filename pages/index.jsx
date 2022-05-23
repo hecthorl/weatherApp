7f11b4d5493b64d720814fb66038ae2de0a94c86
hector vargas <hlvm03@gmail.com>
2022-05-23 14:26:55 -0500

    animate presence functionality

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,14 +2,16 @@ import Head from 'next/head'
 import { Box } from '@mantine/core'
 import getForecast from 'services/getForecast'
 import getIpClient from 'services/getIpClient'
-import Weather from 'components/Weather'
-import TabBar from 'components/TabBar'
-import WeatherHour from 'components/WeatherHour'
+import HeroComponent from 'components/HeroComponent'
+import { AnimatePresence } from 'framer-motion'
+import SearchView from 'components/SearchView'
+import useBoolean from 'hooks/useBoolean'
 
 export default function Home({ data = {} }) {
    const background = data.isDay
       ? 'linear-gradient(226deg, #5181ff, #c159ec)'
       : 'url(/sky_noc.png)'
+   const [isThere, upDateState] = useBoolean(true)
    return (
       <>
          <Head>
@@ -34,90 +36,13 @@ export default function Home({ data = {} }) {
                   position: 'relative'
                }}
             >
-               <Box
-                  sx={{
-                     position: 'absolute',
-                     width: '100%',
-                     height: 325,
-                     borderRadius: 44,
-                     background:
-                        'linear-gradient(132deg, rgba(46,51,90,1) 0%, rgba(28,27,51,1) 61%)',
-                     bottom: -40,
-                     overflow: 'hidden',
-                     borderTop: '1px solid #956bb7'
-                  }}
-               >
-                  <Box
-                     sx={{
-                        position: 'relative',
-                        width: '100%',
-                        height: '100%',
-                        ':after': {
-                           content: '""',
-                           position: 'absolute',
-                           width: '100%',
-                           height: 84,
-                           backgroundColor: '#E0D9FF',
-                           filter: 'blur(10px)',
-                           top: -86,
-                           borderRadius: '50%'
-                        },
-                        ':before': {
-                           content: '""',
-                           position: 'absolute',
-                           width: '100%',
-                           height: 84,
-                           backgroundColor: '#C427FB',
-                           filter: 'blur(60px)',
-                           top: -74,
-                           borderRadius: '50%'
-                        }
-                     }}
-                  >
-                     <WeatherHour />
-                     <Box
-                        sx={{
-                           position: 'absolute',
-                           height: 5,
-                           width: 48,
-                           background: '#1f1d47ad',
-                           borderRadius: 999999,
-                           top: 8,
-                           left: 'calc(50% - 24px)',
-                           zIndex: 55555
-                        }}
-                     />
-                  </Box>
-               </Box>
-               <TabBar />
-               <Box
-                  sx={{
-                     display: 'flex',
-                     flexDirection: 'column',
-                     width: '100%'
-                  }}
-               >
-                  <Weather
-                     location={data.location.name}
-                     weatherDesc={data.shortDesc}
-                     temperature={data.temp}
-                  />
-                  <Box
-                     sx={{ flex: 1, display: 'flex', justifyContent: 'center' }}
-                  >
-                     <Box
-                        sx={{
-                           width: 200,
-                           height: 200,
-                           background:
-                              'linear-gradient(270deg, #c159ec, #5181ff)',
-                           borderRadius: 44
-                        }}
-                        component="img"
-                        src={`/wi/${data.iconNumber}.svg`}
-                     />
-                  </Box>
-               </Box>
+               <AnimatePresence>
+                  {isThere ? (
+                     <HeroComponent data={data} onClick={upDateState.toggle} />
+                  ) : (
+                     <SearchView onClick={upDateState.toggle} />
+                  )}
+               </AnimatePresence>
             </Box>
          </Box>
       </>
